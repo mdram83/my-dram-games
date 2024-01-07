8ba2f0985022cc005c57306fc3ecc1d4af56ad3c
mdram83 <michal.dramowicz@gmail.com>
2024-01-08 00:22:44 +0100

    Adjusted configuration to expose websocket port

--- application/resources/js/bootstrap.js
+++ application/resources/js/bootstrap.js
@@ -23,6 +23,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
     forceTLS: false,
