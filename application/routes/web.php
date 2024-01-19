c302860be3c53e6ab04f71bdcbdd2791136dd295
mdram83 <michal.dramowicz@gmail.com>
2024-01-19 16:03:33 +0100

    Remove dashboard route and related views - not required.

--- application/routes/web.php
+++ application/routes/web.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('home'))->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
