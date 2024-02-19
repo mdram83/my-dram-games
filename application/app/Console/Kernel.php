a62f20aca24820fc8dcb4bcacaf90a4a3a8e8598
mdram83 <michal.dramowicz@gmail.com>
2024-02-19 22:22:13 +0100

    Added PlayerAnonymousEloquent.php prunable() method to remove Anonymous users used later than cookie lifetime (cookie holding anonymous player hash).

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\GameCore\Player\PlayerAnonymousEloquent;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('model:prune', [
+             '--model' => [PlayerAnonymousEloquent::class],
+         ])
+             ->everyFifteenMinutes()
+             ->environments(['production'])
+         ;
     }
 
     /**
--- application/app/Models/GameCore/Player/PlayerAnonymousEloquent.php
+++ application/app/Models/GameCore/Player/PlayerAnonymousEloquent.php
@@ -2,19 +2,27 @@
 
 namespace App\Models\GameCore\Player;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class PlayerAnonymousEloquent extends Authenticatable implements PlayerAnonymous
 {
     use HasFactory;
     use HasUuids;
+    use Prunable;
 
     public $incrementing = false;
     protected $keyType = 'string';
 
     protected $fillable = ['hash', 'name'];
+
+    public function prunable(): Builder
+    {
+        return static::where('updated_at', '<=', now()->subMinutes(config('player.playerHashExpiration')));
+    }
 
     public function getId(): int|string
     {
