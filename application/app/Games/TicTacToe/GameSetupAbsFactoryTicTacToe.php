d4c4b9241522c737ec4c8aa2e6ee09d629a006ac
mdram83 <michal.dramowicz@gmail.com>
2024-03-07 18:29:39 +0100

    Remove unnecessary annotation

--- application/app/Games/TicTacToe/GameSetupAbsFactoryTicTacToe.php
+++ application/app/Games/TicTacToe/GameSetupAbsFactoryTicTacToe.php
@@ -4,13 +4,9 @@ namespace App\Games\TicTacToe;
 
 use App\GameCore\GameSetup\GameSetup;
 use App\GameCore\GameSetup\GameSetupAbsFactory;
-use App\GameCore\GameSetup\GameSetupException;
 
 class GameSetupAbsFactoryTicTacToe implements GameSetupAbsFactory
 {
-    /**
-     * @throws GameSetupException
-     */
     public function create(): GameSetup
     {
         return new GameSetupTicTacToe();
