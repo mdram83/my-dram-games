37c23a7af60cc93adba13680ed1679a920ce4d2a
mdram83 <michal.dramowicz@gmail.com>
2024-03-24 19:36:39 +0100

    wip. GamePlay frontend adjustment of Board placeholder div

--- application/resources/js/src/games/tic-tac-toe/index.jsx
+++ application/resources/js/src/games/tic-tac-toe/index.jsx
@@ -17,20 +17,22 @@ createRoot(rootElement).render(
     <div className="relative w-full h-full">
 
         {/*--- Menu ---*/}
-        <div className="fixed top-0 w-full h-16 sm:h-24 bg-gray-800 border border-solid border-blue-300">
+        <div className="fixed top-0 w-full h-16 sm:h-24 bg-gray-800 z-10">
             <Menu gameInvite={gameInvite} />
         </div>
 
         {/*--- Board ---*/}
-        <div className="fixed top-0 mt-16 sm:mt-20 mb-36 sm:top-24 w-full h-1/2 border-4 border-solid border-red-500">
-            {/*relative mt-16 sm:mt-24 p-4*/}
+
+        {/*Alternative div positioning options*/}
+        {/*<div className="relative mt-16 sm:mt-24 w-full">*/}
+        <div className="fixed mt-16 sm:mt-24 w-full h-[74%] sm:h-[77%] border-4 border-solid border-red-500">
+
             {/*TODO Board to play goes here*/}
-            <p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p>
-            <p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p><p>Content</p>
+
         </div>
 
         {/*--- Status Bar ---*/}
-        <div className="fixed bottom-0 w-full h-max px-4 sm:px-1 py-2 sm:py-4 bg-gray-800 border-4 border-solid border-yellow-500">
+        <div className="fixed bottom-0 w-full h-max px-4 sm:px-1 py-2 sm:py-4 bg-gray-800">
             <StatusBarTicTacToe activePlayer={situation.activePlayer} characters={situation.characters} />
         </div>
 
