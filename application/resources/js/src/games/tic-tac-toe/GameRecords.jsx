172b393ca9f2e920e6a27c302160b625a157a8d1
mdram83 <michal.dramowicz@gmail.com>
2024-04-02 15:30:53 +0200

    Adjust GameRecords.jsx to handle Draw results properly

--- application/resources/js/src/games/tic-tac-toe/GameRecords.jsx
+++ application/resources/js/src/games/tic-tac-toe/GameRecords.jsx
@@ -4,14 +4,20 @@ import {configTicTacToe} from "./configTicTacToe.jsx";
 const GameRecords = (gameRecords) => {
 
     const winnerRecord = Object.values(gameRecords).filter((record) => record.isWinner === true)[0];
-    const winnerName = winnerRecord.player;
-    const winnerCharacterAvatar = configTicTacToe[winnerRecord.score.character].avatar;
-
+    const hasWinner = winnerRecord !== undefined;
+    const winnerName = hasWinner ? winnerRecord.player : undefined;
+    const winnerCharacterAvatar = hasWinner ? configTicTacToe[winnerRecord.score.character].avatar : undefined;
 
     return (
         <>
-            <i className='fa fa-trophy text-amber-500'></i>
-            <span> {winnerName} ({winnerCharacterAvatar})</span>
+            {hasWinner &&
+                <>
+                    <i className='fa fa-trophy text-amber-500'></i>
+                    <span> {winnerName} ({winnerCharacterAvatar})</span>
+                </>
+            }
+
+            {!hasWinner && <span>Game finished with draw.</span>}
         </>
     );
 }
