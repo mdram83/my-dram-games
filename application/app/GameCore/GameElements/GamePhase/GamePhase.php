082af637d4e005f6f3553faaa14af3416d9b43e1
mdram83 <michal.dramowicz@gmail.com>
2024-04-14 20:48:00 +0200

    GamePhase.php interface.

---
+++ application/app/GameCore/GameElements/GamePhase/GamePhase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GameCore\GameElements\GamePhase;
+
+use App\GameCore\GameElements\GameMove\GameMove;
+
+interface GamePhase
+{
+    public function getKey(): string;
+    public function getName(): string;
+    public function getDescription(): string;
+    public function getNextPhase(bool $lastPlayer): GamePhase;
+    public function getMoveResults(GameMove $move): array;
+}
