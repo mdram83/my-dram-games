f4c0bfe5ae4d9379c223d2306957754962deaee7
mdram83 <michal.dramowicz@gmail.com>
2024-05-20 21:47:33 +0200

    wip Move event function in index.jsx. Add seatAssignment.jsx function

---
+++ application/resources/js/src/games/thousand/seatAssignment.jsx
@@ -0,0 +1,25 @@
+import {useThousandStore} from "./useThousandStore.jsx";
+
+export const seatAssignment = () => {
+
+    console.log('call seatAssignment');
+    const orderedPlayers = useThousandStore.getState().situation.orderedPlayers;
+    const yourSeat = orderedPlayers[window.MyDramGames.player.name].seat;
+    const names = Object.getOwnPropertyNames(orderedPlayers);
+    const numberOfPlayers = names.length;
+
+    const seats = new Array(4);
+    names.forEach((name, index, names) => {
+        seats[orderedPlayers[name].seat] = name;
+    });
+
+    const leftHandSeat = (yourSeat % numberOfPlayers) + 1;
+    const rightHandSeat = ((yourSeat + numberOfPlayers - 2) % numberOfPlayers) + 1;
+    const frontSeat = numberOfPlayers === 4 ? (((yourSeat + 1) % 4) + 1) : undefined;
+
+    return [
+        seats[leftHandSeat],
+        numberOfPlayers === 4 ? seats[frontSeat] : undefined,
+        seats[rightHandSeat],
+    ];
+}
