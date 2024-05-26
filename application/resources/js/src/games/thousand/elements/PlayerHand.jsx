32f3fd17f33f77359a1937b016856b277ff76eb4
mdram83 <michal.dramowicz@gmail.com>
2024-05-26 19:51:06 +0200

    wip Move event function in index.jsx. Add necessary components.

--- application/resources/js/src/games/thousand/elements/PlayerHand.jsx
+++ application/resources/js/src/games/thousand/elements/PlayerHand.jsx
@@ -8,20 +8,12 @@ export const PlayerHand = ({playerName}) => {
 
     const hand = useThousandStore(state => state.situation.orderedPlayers[playerName].hand);
 
-    console.log(hand);
-
-    // FIXME I dont understand why do I need to add empty cell to grid to keep it in one row...
-    // TODO anyway I should now add Spring and sortable elements instead. Let's see how it works then (Maybe without a grid, depends how Spring handles that)
-    const gridClassName = 'grid grid-cols-' + (hand.length + 1).toString();
-
+    // TODO redo and use DraggableList example from react spring
     const renderElements = () => {
-        const elements = [];
-        hand.forEach((cardKey) => elements.push(<PlayingCard key={cardKey} cardKey={cardKey} />));
-        // return hand.map((cardKey) => <PlayingCard key={cardKey} cardKey={cardKey} />);
-        return elements;
+        return hand.map((cardKey) => <PlayingCard key={cardKey} cardKey={cardKey} />);
     }
 
     return (
-        <div className={'flex ' + gridClassName}>{renderElements()}</div>
+        <div className="flex items-center">{renderElements()}</div>
     );
 }
