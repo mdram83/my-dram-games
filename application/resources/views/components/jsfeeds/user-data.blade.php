cdcf212046eb4940b873985a6fc328968c7a6493
mdram83 <michal.dramowicz@gmail.com>
2024-08-05 14:02:11 +0200

    adjust deployment procedure to clear cache to solve db connection timeout issues.

--- application/resources/views/components/jsfeeds/user-data.blade.php
+++ application/resources/views/components/jsfeeds/user-data.blade.php
@@ -9,11 +9,6 @@
 
             // TODO I am exposing id to connect player to indivitual player private channels. Is it ok to expose it?
             'id': @if(isset($player)) `{{ $player->getId() }}` @else {{ 'undefined' }} @endif,
-
-            // TODO temp to check env vars issues
-            'app': {
-                'env': `{{ $appEnv }}`,
-            }
         }
     }
 </script>
