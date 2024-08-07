6fc56cae60f19cc05663a8b1666f5520bcb240fd
mdram83 <michal.dramowicz@gmail.com>
2024-08-07 16:33:05 +0200

    Dynamic hero section with configurable inputs through blade component hero-item.blade.php. Added basic assets.

---
+++ application/resources/views/components/template/hero-item.blade.php
@@ -0,0 +1,22 @@
+@props(['label'=>null, 'title', 'description', 'img', 'link'=>null, 'button'=>null])
+
+<div class="hero__items set-bg" data-setbg="{{ $img }}">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="hero__text">
+
+                @isset($label)
+                    <div class="label">{{ $label }}</div>
+                @endisset
+
+                <h2>{{ $title }}</h2>
+                <p>{{ $description }}</p>
+
+                @isset($link)
+                    <a href="{{ $link }}"><span>{{ $button ?? 'More...' }}</span> <i class="fa fa-angle-right"></i></a>
+                @endisset
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/resources/views/components/template/hero-section.blade.php
@@ -0,0 +1,28 @@
+<!-- Hero Section Begin -->
+<section class="hero">
+    <div class="container">
+        <div class="hero__slider owl-carousel">
+
+            <x-template.hero-item label="News"
+                                  title="Welcome!"
+                                  description="We're live! Play your favorite games online"
+                                  img="{{ asset('img/template/gaming-room.jpg') }}" />
+
+            <x-template.hero-item label="Paper Game"
+                                  title="Tic-Tac-Toe game live!"
+                                  description="Play famous tic-tac-toe game"
+                                  img="{{ asset('img/games/tic-tac-toe.jpg') }}"
+                                  link="/games/tic-tac-toe"
+                                  button="Play Now" />
+
+            <x-template.hero-item label="Playing Cards"
+                                  title="Thousand Schnapsen"
+                                  description="1000 card games now available"
+                                  img="{{ asset('img/games/thousand.jpg') }}"
+                                  link="/games/thousand"
+                                  button="Play Now" />
+
+        </div>
+    </div>
+</section>
+<!-- Hero Section End -->
