1ac6f294892a4a44a00edc644e326b2f94be4d47
mdram83 <michal.dramowicz@gmail.com>
2024-08-12 21:47:48 +0200

    adjust join-redirect.blade.php view content visible for user.

--- application/resources/views/join-redirect.blade.php
+++ application/resources/views/join-redirect.blade.php
@@ -6,17 +6,18 @@
     <section class="login spad sm:-mt-[40px] -mt-[100px] pb-0">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
+{{--                <div class="col-lg-6">--}}
                     <div class="login__register">
-                        <h3>Where do you want to go?</h3>
+                        <h3 class="w-full text-center">We are redirecting you to game lobby</h3>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="login__register">
-                        <a href="javascript:history.back()" class="primary-btn mr-2 mb-2">Go Back</a>
-                        <a href="/" class="primary-btn">Home Page</a>
-                    </div>
-                </div>
+{{--                <div class="col-lg-6">--}}
+{{--                    <div class="login__register">--}}
+{{--                        <a href="javascript:history.back()" class="primary-btn mr-2 mb-2">Go Back</a>--}}
+{{--                        <a href="/" class="primary-btn">Home Page</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
             </div>
         </div>
     </section>
