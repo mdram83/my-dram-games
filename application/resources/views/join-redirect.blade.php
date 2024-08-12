1c9cf64a87f15f9e131c77dd0392bd7087dd15dd
mdram83 <michal.dramowicz@gmail.com>
2024-08-12 21:53:33 +0200

    adjust join-redirect.blade.php view content visible for user (remove backup comments)

--- application/resources/views/join-redirect.blade.php
+++ application/resources/views/join-redirect.blade.php
@@ -7,17 +7,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-{{--                <div class="col-lg-6">--}}
                     <div class="login__register">
                         <h3 class="w-full text-center">We are redirecting you to game lobby</h3>
                     </div>
                 </div>
-{{--                <div class="col-lg-6">--}}
-{{--                    <div class="login__register">--}}
-{{--                        <a href="javascript:history.back()" class="primary-btn mr-2 mb-2">Go Back</a>--}}
-{{--                        <a href="/" class="primary-btn">Home Page</a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
             </div>
         </div>
     </section>
