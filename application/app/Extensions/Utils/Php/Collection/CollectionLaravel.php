d05b29b4a804249cd3d14730d185b15966adf2e9
mdram83 <michal.dramowicz@gmail.com>
2024-08-28 14:04:13 +0200

    Bump mydramgames-utils version and add Extensions class - CollectionLaravel.php including tests.

---
+++ application/app/Extensions/Utils/Php/Collection/CollectionLaravel.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Extensions\Utils\Php\Collection;
+
+use MyDramGames\Utils\Exceptions\CollectionException;
+use MyDramGames\Utils\Php\Collection\Collection;
+use Illuminate\Support\Collection as IlluminateCollection;
+use MyDramGames\Utils\Php\Collection\CollectionTrait;
+
+/**
+ * To support specific collection types, define TYPE_CLASS or TYPE_PRIMITIVE values in child class.
+ * To support specific key mode, define KEY_MODE value (loose, forced, method) in child class.
+ * See details in CollectionTrait PHPDoc
+ */
+class CollectionLaravel implements Collection
+{
+    use CollectionTrait;
+
+    protected IlluminateCollection $items;
+
+    /**
+     * @inheritDoc
+     */
+    final public function count(): int
+    {
+        return $this->items->count();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function isEmpty(): bool
+    {
+        return $this->items->isEmpty();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function exist(mixed $key): bool
+    {
+        return $this->items->has($key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function toArray(): array
+    {
+        return $this->items->all();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function each(callable $callback): static
+    {
+        $items = $this->items->map($callback);
+        return $this->reset($items->all());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function filter(callable $callback): static
+    {
+        $items = $this->items->filter($callback);
+        return new static($items->all());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function random(): mixed
+    {
+        $this->validateNotEmpty();
+        return $this->items->random();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function getOne(mixed $key): mixed
+    {
+        $this->validateExists($key);
+        return $this->items->get($key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function removeOne(mixed $key): void
+    {
+        $this->validateExists($key);
+        $this->items->forget($key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function removeAll(): void
+    {
+        $this->items = new IlluminateCollection();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function pullFirst(): mixed
+    {
+        $this->validateNotEmpty();
+        return $this->items->shift();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final public function pullLast(): mixed
+    {
+        $this->validateNotEmpty();
+        return $this->items->pop();
+    }
+
+    /**
+     * @inheritDoc
+     * @throws CollectionException
+     */
+    final public function clone(): static
+    {
+        return new self($this->items->toArray());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getItemKey(mixed $item): mixed
+    {
+        return null;
+    }
+
+    protected function insert(mixed $item, mixed $key = null): void
+    {
+        isset($key) ? $this->items->put($key, $item) : $this->items->push($item);
+    }
+}
