7a9185dcfd3451728e8ef1d0466027fbc2e6e220
mdram83 <michal.dramowicz@gmail.com>
2024-10-03 05:54:53 +0200

    initial frontend setup

---
+++ application/resources/js/src/games/netrunners/elements/players/CharactersGrid.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {useNetrunnersStore} from "../../useNetrunnersStore.jsx";
+import {GridAvatar} from "./GridAvatar.jsx";
+
+export const CharactersGrid = () => {
+
+    const phaseKey = useNetrunnersStore(state => state.situation.phase.key);
+    const isPhaseValid = phaseKey === 'character';
+
+    const gridItems = (new Array(6).fill(null)).map((_, index) => {
+        return (
+            <div key={index} className=' flex items-center justify-center border border-solid border-green-500 '>
+                <GridAvatar gridKey={index} />
+            </div>
+        );
+    });
+
+    return (
+        isPhaseValid &&
+        <div className='w-full h-full flex justify-center'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 h-[88%] sm:h-full w-full sm:w-[60%]'>
+                {gridItems}
+            </div>
+        </div>
+    );
+}
