7380953c3816d91d42b863c92754d6ce6e81980d
mdram83 <michal.dramowicz@gmail.com>
2024-10-24 17:39:40 +0200

    WIP. temp images for nodes.

--- application/resources/js/src/games/netrunners/configNetrunners.jsx
+++ application/resources/js/src/games/netrunners/configNetrunners.jsx
@@ -44,7 +44,17 @@ export const configNetrunners = {
         },
         location: {
             imageCoverM: 'url(https://images.pexels.com/photos/343457/pexels-photo-343457.jpeg)',
-            CrossroadRegular: 'url(https://images.pexels.com/photos/19804230/pexels-photo-19804230/free-photo-of-top-view-of-a-large-intersection.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+            Start: 'url(https://images.pexels.com/photos/19804230/pexels-photo-19804230/free-photo-of-top-view-of-a-large-intersection.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+            Charger: 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowyb-22-zakaz-skr_cania-w-prawo_jpg.webp)',
+            CrossroadRegular: 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-c-12-ruch-okr_ny-_rondo__jpg.webp)',
+            CrossroadEncounter : 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-a-8-skrzy_owanie-o-ruchu-okr_nym_1_jpg.webp)',
+            StraightRegular: 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-c-5-nakaz-jazdy-prosto_jpg.webp)',
+            StraightEncounter : 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-a-3-niebezpieczne-zakr_ty---pierwszy-w-prawo_1_jpg.webp)',
+            StraightPortal : 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-a-20-odcinek-jezdni-o-ruchu-dwukierunkowym_jpg.webp)',
+            TurnRegular: 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-c-2-nakaz-jazdy-w-prawo-za-znakiem_jpg.webp)',
+            TurnEncounter : 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-a-1_1_jpg.webp)',
+            TShapeRegular : 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-c-6-nakaz-jazdy-prosto-lub-w-prawo_1_jpg.webp)',
+            TShapeEncounter: 'url(https://www.matech.com.pl/media/amasty/webp/catalog/product/cache/067e632235bbc3ec7310f5f350b3960e/z/n/znak-drogowy-a-6b-skrzy_owanie-z-drog_-podporz_dkowan_-wyst_puj_c_-po-prawej-stronie_1_jpg.webp)',
         }
     },
     engine: {
