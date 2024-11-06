31e0004503301aec29de4538caf908211e386ec8
mdram83 <michal.dramowicz@gmail.com>
2024-11-06 17:36:29 +0100

    Add ActionButton.jsx to enable actual recharge from RechargeInfo.jsx

---
+++ application/resources/js/src/games/netrunners/elements/actions/ActionButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const ActionButton = ({onClick, label}) => {
+
+    console.log('ActionButton');
+
+    const classButtonDiv
+        = ' w-[25vh] h-[6vh] flex justify-center items-center '
+        + ' border border-solid border-[0.5vh] rounded-[1.5vh] border-orange-500 bg-neutral-200 hover:bg-neutral-300 '
+        + ' shadow-actionSm hover:shadow-actionLg cursor-pointer '
+        + ' font-sans font-bold antialiased text-[3vh] uppercase text-black ';
+
+    return (
+        <div className={classButtonDiv} onClick={onClick}>
+            <div>{label}</div>
+        </div>
+    );
+}
--- application/resources/js/src/games/netrunners/elements/actions/RechargeInfo.jsx
+++ application/resources/js/src/games/netrunners/elements/actions/RechargeInfo.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import {useNetrunnersStore} from "../../useNetrunnersStore.jsx";
+import {submitMove} from "../../submitMove.jsx";
+import {useGamePlayStore} from "../../../../game-core/game-play/useGamePlayStore.jsx";
+import {ActionButton} from "./ActionButton.jsx";
 
 export const RechargeInfo = () => {
 
-    console.log('actions/RechargeInfo');
+    console.log('RechargeInfo');
 
+    const gamePlayId = useGamePlayStore(state => state.gamePlayId);
+    const setMessage = useGamePlayStore((state) => state.setMessage);
     const display = useNetrunnersStore(state => state.rechargeInfoScreen.display);
     const setRechargeInfoScreen = useNetrunnersStore(state => state.setRechargeInfoScreen);
 
     const playerName = MyDramGames.player.name;
 
+    const onClick = () => {
+        submitMove({recharge: true}, gamePlayId , setMessage, 'recharge');
+        setRechargeInfoScreen(false);
+    }
+
     const classDivContainer = ' h-[70vh] w-[50vh] sm:w-[100vh] mt-[2vh] shadow-xl shadow-black text-white '
         + ' border-[0.3vh] sm:border-[0.25vh] border-solid border-fuchsia-500 rounded-[2vh] ';
     const classDivDetails = ' p-[4%] w-[92%] h-[95%] ';
+    const classDivSpan = ' col-span-2 sm:col-span-1 ';
 
     return (
         <>
@@ -39,21 +50,27 @@ export const RechargeInfo = () => {
 
 
                     {/*DETAILS SECTION*/}
-                    <div className='flex grid grid-cols-1 gap-0 w-full h-[65.5vh] bg-neutral-800/95 rounded-b-[2vh]'>
+                    <div className='flex grid grid-cols-2 gap-0 w-full h-[65.5vh] bg-neutral-800/95 rounded-b-[2vh]'>
 
                         {/*BATTERY SECTION*/}
-                        <div className={classDivDetails}>
-                            <div>Battery Info</div>
+                        <div className={classDivDetails + classDivSpan}>
+                            <div className=' flex justify-center items-center w-full h-full '>
+                                <div>Battery Animation</div>
+                            </div>
                         </div>
 
                         {/*HACK SECTION*/}
-                        <div className={classDivDetails}>
-                            <div>Hack Info</div>
+                        <div className={classDivDetails + classDivSpan}>
+                            <div className=' flex justify-center items-center w-full h-full '>
+                                <div>Hack Animation</div>
+                            </div>
                         </div>
 
                         {/*BUTTON SECTION*/}
-                        <div className={classDivDetails}>
-                            <div>Button</div>
+                        <div className={classDivDetails + ' col-span-2 '}>
+                            <div className=' flex justify-center items-center w-full h-full '>
+                                <ActionButton onClick={onClick} label='Recharge' />
+                            </div>
                         </div>
 
                     </div>
