61285cee32faf4b78394ab4a49a621bdb67b1e45
mdram83 <michal.dramowicz@gmail.com>
2024-11-26 09:02:34 +0100

    Adjust console to show remaining encounters

--- application/resources/js/src/games/netrunners/elements/console/Console.jsx
+++ application/resources/js/src/games/netrunners/elements/console/Console.jsx
@@ -9,15 +9,16 @@ export const Console = () => {
     const activePlayer = useNetrunnersStore(state => state.situation.activePlayer);
     const remainingMoves = useNetrunnersStore(state => state.situation.players[activePlayer].remainingMoves);
     const remainingNodes = useNetrunnersStore(state => state.situation.remainingNodes);
+    const remainingEnconters = useNetrunnersStore(state => state.situation.remainingEncounters);
 
-    const text = `  ${activePlayer} | Moves ${remainingMoves} | Nodes ${remainingNodes}  `;
+    const text = `  ${activePlayer} | Moves ${remainingMoves} | Nodes ${remainingNodes} | Encounters ${remainingEnconters} `;
     const displayText = useTypewriter(text, 10, 1000);
 
     return (
-        <div className='size-full p-[1vh] bg-neutral-900 leading-none sm:leading-tight font-mono text-lime-500 text-[2.5vh] sm:text-[2.5vh]'>
-            <div>> campaign stats --refresh</div>
+        <div className='size-full p-[1vh] bg-neutral-900 leading-tight sm:leading-tight font-mono text-lime-500 text-[2.5vh] sm:text-[2.5vh]'>
+            <div> --- campaign stats ---</div>
             <div>{displayText}</div>
-            <div>>_</div>
+            <div className='hidden sm:flex'>>_</div>
         </div>
     );
 }
