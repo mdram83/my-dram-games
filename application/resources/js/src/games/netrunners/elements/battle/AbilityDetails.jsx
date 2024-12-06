cf860c383ce2524492229c0fdf03624042766dc8
mdram83 <michal.dramowicz@gmail.com>
2024-12-06 15:50:31 +0100

    Adjust AbilityDetails.jsx to use only power abilities.

--- application/resources/js/src/games/netrunners/elements/battle/AbilityDetails.jsx
+++ application/resources/js/src/games/netrunners/elements/battle/AbilityDetails.jsx
@@ -10,37 +10,37 @@ export const AbilityDetails = ({player, addClass}) => {
     const abilityPower = useNetrunnersStore(state => state.situation.battle.abilityPower);
     const ability = useNetrunnersStore(state => state.situation.battle.ability);
 
-    const hasAbility = ability !== null;
-    const abilityKey = hasAbility ? ability.key : undefined;
-    const abilityInUse = hasAbility ? ability.inUse : undefined;
-
-    const classDiv = ' h-[70%] bg-center bg-no-repeat bg-cover aspect-square rounded-lg mr-[2vh] border-[0.4vh] border-solid '
-        + ' border-neutral-500 ';
-
-    const styles = !hasAbility
-        ? {backgroundColor: 'rgb(23 23 23)'}
-        : {
-            backgroundImage: configNetrunners.covers.hacked, // add proper picture when you have one and when you test further
-            filter: (abilityInUse ? 'grayscale(0)' : 'grayscale(100%)'),
-        }
-
-
     /* Active:
     AttackLosingPower
 
     Passive:
     WinDraw
     FirstMoveAttackBonus
-
-    Dices:
-    ReRollDices
-    ReRollOne
      */
+
+    const powerAbilities = ['AttackLosingPower', 'WinDraw', 'FirstMoveAttackBonus'];
+
+    const hasPowerAbility = ability !== null && powerAbilities.includes(ability.key);
+    const abilityKey = hasPowerAbility ? ability.key : undefined;
+    const abilityInUse = hasPowerAbility ? ability.inUse : undefined;
+
+    const classDiv = ' h-[70%] bg-center bg-no-repeat bg-cover aspect-square rounded-lg mr-[2vh] border-[0.4vh] border-solid '
+        + ' border-neutral-500 ';
+
+    const classNoPowerAbilities = ' font-sans text-neutral-600 text-[2vh] uppercase ';
+
+    const styles = !hasPowerAbility
+        ? {backgroundColor: 'rgb(23 23 23)'}
+        : {
+            backgroundImage: configNetrunners.covers.abilities[abilityKey],
+            filter: (abilityInUse ? 'grayscale(0)' : 'grayscale(100%)'),
+        }
 
     return (
         <div className={addClass}>
             <div className='flex h-full items-center justify-center'>
-                <div className={classDiv} style={styles}></div>
+                {hasPowerAbility && <div className={classDiv} style={styles}></div>}
+                {!hasPowerAbility && <div className={classNoPowerAbilities}>No power ability available</div>}
             </div>
             <PowerDetails power={abilityPower}/>
         </div>
