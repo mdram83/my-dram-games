6e7897e84522c4fb251ac424a0709e96eb130c68
mdram83 <michal.dramowicz@gmail.com>
2024-12-06 16:28:05 +0100

    Handle more abilities.

--- application/resources/js/src/games/netrunners/elements/battle/AbilityDetails.jsx
+++ application/resources/js/src/games/netrunners/elements/battle/AbilityDetails.jsx
@@ -2,47 +2,59 @@ import {configNetrunners} from "../../configNetrunners.jsx";
 import React from "react";
 import {useNetrunnersStore} from "../../useNetrunnersStore.jsx";
 import {PowerDetails} from "./PowerDetails.jsx";
+import {submitMove} from "../../submitMove.jsx";
+import {useGamePlayStore} from "../../../../game-core/game-play/useGamePlayStore.jsx";
 
 export const AbilityDetails = ({player, addClass}) => {
 
     console.log('AbilityDetails', player);
 
+    const gamePlayId = useGamePlayStore(state => state.gamePlayId);
+    const setMessage = useGamePlayStore((state) => state.setMessage);
+    const yourTurn = useNetrunnersStore(state => state.yourTurn);
+
     const abilityPower = useNetrunnersStore(state => state.situation.battle.abilityPower);
     const ability = useNetrunnersStore(state => state.situation.battle.ability);
 
-    /* Active:
-    AttackLosingPower
-
-    Passive:
-    WinDraw
-    FirstMoveAttackBonus
-     */
-
     const powerAbilities = ['AttackLosingPower', 'WinDraw', 'FirstMoveAttackBonus'];
-
     const hasPowerAbility = ability !== null && powerAbilities.includes(ability.key);
     const abilityKey = hasPowerAbility ? ability.key : undefined;
     const abilityInUse = hasPowerAbility ? ability.inUse : undefined;
+    const canTogglePowerAbility = yourTurn && hasPowerAbility && abilityKey === 'AttackLosingPower';
+
+    const handleAbility = () => {
+        if (canTogglePowerAbility) {
+            submitMove({ability: !abilityInUse}, gamePlayId , setMessage, 'battle');
+        }
+    }
 
     const classDiv = ' h-[70%] bg-center bg-no-repeat bg-cover aspect-square rounded-lg mr-[2vh] border-[0.4vh] border-solid '
-        + ' border-neutral-500 ';
+        + (canTogglePowerAbility ? ' border-orange-500 shadow-actionSm hover:shadow-actionLg hover:cursor-pointer ' : ' border-neutral-500 ');
 
     const classNoPowerAbilities = ' font-sans text-neutral-600 text-[2vh] uppercase ';
 
     const styles = !hasPowerAbility
-        ? {backgroundColor: 'rgb(23 23 23)'}
+        ? {}
         : {
             backgroundImage: configNetrunners.covers.abilities[abilityKey],
             filter: (abilityInUse ? 'grayscale(0)' : 'grayscale(100%)'),
-        }
+        };
 
     return (
         <div className={addClass}>
+
             <div className='flex h-full items-center justify-center'>
-                {hasPowerAbility && <div className={classDiv} style={styles}></div>}
+
+                {hasPowerAbility && <div className={classDiv} onClick={handleAbility}>
+                    <div className='h-full bg-center bg-no-repeat bg-cover aspect-square rounded-lg' style={styles}></div>
+                </div>}
+
                 {!hasPowerAbility && <div className={classNoPowerAbilities}>No power ability available</div>}
+
             </div>
+
             <PowerDetails power={abilityPower}/>
+
         </div>
     );
 }
--- application/resources/js/src/games/netrunners/elements/battle/DiceDetails.jsx
+++ application/resources/js/src/games/netrunners/elements/battle/DiceDetails.jsx
@@ -2,19 +2,38 @@ import {configNetrunners} from "../../configNetrunners.jsx";
 import React from "react";
 import {useNetrunnersStore} from "../../useNetrunnersStore.jsx";
 import {PowerDetails} from "./PowerDetails.jsx";
+import {useGamePlayStore} from "../../../../game-core/game-play/useGamePlayStore.jsx";
+import {submitMove} from "../../submitMove.jsx";
 
 export const DiceDetails = ({player, addClass}) => {
 
     console.log('DiceDetails', player);
 
+    const gamePlayId = useGamePlayStore(state => state.gamePlayId);
+    const setMessage = useGamePlayStore((state) => state.setMessage);
+    const yourTurn = useNetrunnersStore(state => state.yourTurn);
+
     const dices = useNetrunnersStore(state => state.situation.battle.dices);
+    const ability = useNetrunnersStore(state => state.situation.battle.ability);
+
+    const diceAbilities = ['ReRollDices', 'ReRollOne'];
+    const hasDiceAbility = ability !== null && diceAbilities.includes(ability.key);
+    const abilityInUse = hasDiceAbility ? ability.inUse : undefined;
+    const canUseDiceAbility = yourTurn && hasDiceAbility && !abilityInUse;
 
     const styleImage = {
         backgroundImage: configNetrunners.covers.diceS,
     };
 
+    const handleAbility = () => {
+        if (canUseDiceAbility) {
+            submitMove({ability: true}, gamePlayId , setMessage, 'battle');
+        }
+    }
+
     const classDiv = ' h-[70%] bg-center bg-no-repeat bg-cover aspect-square rounded-lg mr-[2vh] border-[0.4vh] border-solid '
-        + ' border-neutral-500 ';
+        + (canUseDiceAbility ? ' border-orange-500 shadow-actionSm hover:shadow-actionLg hover:cursor-pointer ' : ' border-neutral-500 ');
+
     //
     // + (yourTurn ? ' border-orange-500 shadow-actionSm hover:shadow-actionLg hover:cursor-pointer ' : ' border-cyan-500 shadow-actionSmOp ')
     //     + addClass;
@@ -22,7 +41,7 @@ export const DiceDetails = ({player, addClass}) => {
     return (
         <div className={addClass}>
             <div className='flex h-full items-center justify-center'>
-                <div className={classDiv} style={styleImage}></div>
+                <div className={classDiv} style={styleImage} onClick={handleAbility}></div>
             </div>
             <PowerDetails power={`${dices[0]} | ${dices[1]}`} addClass=' text-[3.5vh] -mr-[4vh] ' />
             <PowerDetails power={dices[0] + dices[1]}/>
