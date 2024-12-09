10731893ef4dce89a596e01e84389970506e72c1
mdram83 <michal.dramowicz@gmail.com>
2024-12-09 19:37:11 +0100

    Cleanup Attribute.jsx styles so it does not appear with undefined class.

--- application/resources/js/src/games/netrunners/elements/misc/Attribute.jsx
+++ application/resources/js/src/games/netrunners/elements/misc/Attribute.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import {useMediaQuery} from "../../../../customHooks/useMediaQuery.jsx";
 
-export const Attribute = ({className, children, sizeVh = 4, sizeVhSm = 7, additionalStyles = {}}) => {
+export const Attribute = ({className, children, sizeVh = 4, sizeVhSm = 7, additionalStyles = {}, classPadding = undefined}) => {
 
     console.log('Attribute', sizeVh, sizeVhSm);
 
     const isSm = useMediaQuery('(min-width: 640px)');
 
-    const classDivBase = className
+    const classDivBase = (className === undefined ? '' : className)
         + ' aspect-square '
         + ' flex justify-center items-center '
         + ' bg-white/50 '
         + ' border-solid rounded-full '
         + ' font-sans font-semibold leading-none ';
+
+    if (classPadding === undefined) {
+        classPadding = ' pb-[10%] sm:pb-[6%] ';
+    }
 
     const styles = {
         height: `${isSm ? sizeVhSm : sizeVh}vh`,
@@ -23,7 +27,7 @@ export const Attribute = ({className, children, sizeVh = 4, sizeVhSm = 7, additi
 
     return (
         <div className={classDivBase} style={styles}>
-            <div className= ' pb-[10%] sm:pb-[6%] '>
+            <div className={classPadding}>
                 {children}
             </div>
         </div>
