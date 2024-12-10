b7341c27b169b473be4850d4f20e61365c1be512
mdram83 <michal.dramowicz@gmail.com>
2024-12-10 09:28:08 +0100

    Finalize BatteryChange.jsx effects.

--- application/resources/js/src/games/netrunners/elements/players/BatteryChange.jsx
+++ application/resources/js/src/games/netrunners/elements/players/BatteryChange.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {animated, useTransition} from "@react-spring/web";
 import {Battery} from "../misc/Battery.jsx";
 import {useNetrunnersStore} from "../../useNetrunnersStore.jsx";
 
@@ -8,21 +9,42 @@ export const BatteryChange = ({playerName}) => {
 
     const points = useNetrunnersStore(state => state.situation.players[playerName].battery);
 
-    // TODO effect to show it for x seconds after the change and then hide.
-    const targetPoints = useNetrunnersStore(state => state.situation.players[playerName].battery);
+    const [prevPoints, setPrevPoints] = useState(points);
+    const [targetPoints, setTargetPoints] = useState(points);
+    const [display, setDisplay] = useState(false);
 
+    const transitions = useTransition(display, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        config: { duration: 200 },
+    });
 
-    return (
+    useEffect(() => {
+        setPrevPoints(targetPoints);
+        setTargetPoints(points);
+        setDisplay(true);
 
-        <div className="absolute size-[80%]">
-            <div className='flex items-center justify-center size-full'>
+        const timeout = setTimeout(() => {
+            setDisplay(false);
+        }, 3000);
 
-                <div className='w-full h-[60%] aspect-square '>
-                    <Battery points={points} targetPoints={targetPoints} smallSize={true}/>
+        return () => clearTimeout(timeout);
+    }, [points]);
+
+    return transitions((styles, item) =>
+
+        item && (
+            <animated.div style={styles} className="absolute size-[80%]">
+                <div className='flex items-center justify-center size-full'>
+
+                    <div className='w-full h-[60%] aspect-square '>
+                        <Battery points={prevPoints} targetPoints={targetPoints} smallSize={true}/>
+                    </div>
+
                 </div>
-
-            </div>
-        </div>
+            </animated.div>
+        )
 
     );
 }
--- application/resources/js/src/games/netrunners/index.jsx
+++ application/resources/js/src/games/netrunners/index.jsx
@@ -94,9 +94,6 @@ controller.getRoot().render(
             </div>
         </div>
 
-        {/*TODO locate and apply battery up/down effect on Player Avatars*/}
-        {/*TODO fix bug that Speerunner ca continue on six when 0 moves remaining*/}
-
         <FlashMessageGamePlay/>
 
     </div>
