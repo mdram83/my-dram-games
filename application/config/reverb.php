b81d218c5032d70328473007c555092adcb85464
mdram83 <michal.dramowicz@gmail.com>
2025-05-09 09:02:59 +0200

    Add fix for peer verification

--- application/config/reverb.php
+++ application/config/reverb.php
@@ -36,6 +36,7 @@ return [
                 'tls' => [
                     'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
                     'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+                    'verify_peer' => false,
                 ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
